624878d103864808eb5cf4b1d395849dfdf61ee1
philtaubert <phil.taubert@cs13-1.ba-leipzig.de>
2016-03-14 19:01:18 +0100

    Spielfeld Koordinaten hinzugefügt

--- MAEDN/src/si/projekt/maedn/GUI.java
+++ MAEDN/src/si/projekt/maedn/GUI.java
@@ -13,16 +13,250 @@ import org.eclipse.swt.widgets.Scale;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.widgets.Canvas;
 
 public class GUI {
 
 	static Shell shlMeadn;
 	static Display display;
-	static HashMap<Integer, Label> spielfiguren = new HashMap<Integer, Label>();
+	static HashMap<Integer, Canvas> spielfiguren = new HashMap<Integer, Canvas>();
 
+	public static void setzeSpielfigur(int spielernummer, int figurnummer, int feldnummer) {
+		Canvas spielfigur = spielfiguren.get(spielernummer * 10 + figurnummer);
+		switch (feldnummer) {
+		case -11:
+			spielfigur.setLocation(50, 610);
+			break;
+		case -12:
+			spielfigur.setLocation(118, 610);
+			break;
+		case -13:
+			spielfigur.setLocation(50, 680);
+			break;
+		case -14:
+			spielfigur.setLocation(118, 680);
+			break;
+
+		case -21:
+			spielfigur.setLocation(50, 10);
+			break;
+		case -22:
+			spielfigur.setLocation(118, 10);
+			break;
+		case -23:
+			spielfigur.setLocation(50, 80);
+			break;
+		case -24:
+			spielfigur.setLocation(118, 80);
+			break;
+			
+		case -31:
+			spielfigur.setLocation(650, 10);
+			break;
+		case -32:
+			spielfigur.setLocation(718, 10);
+			break;
+		case -33:
+			spielfigur.setLocation(650, 80);
+			break;
+		case -34:
+			spielfigur.setLocation(718, 80);
+			break;
+			
+		case -41:
+			spielfigur.setLocation(650, 610);
+			break;
+		case -42:
+			spielfigur.setLocation(718, 610);
+			break;
+		case -43:
+			spielfigur.setLocation(650, 680);
+			break;
+		case -44:
+			spielfigur.setLocation(718, 680);
+			break;
+			
+		case 10:
+			spielfigur.setLocation(318, 680);
+			break;
+		case 11:
+			spielfigur.setLocation(318, 610);
+			break;
+		case 12:
+			spielfigur.setLocation(318, 547);
+			break;
+		case 13:
+			spielfigur.setLocation(318, 479);
+			break;
+		case 14:
+			spielfigur.setLocation(318, 420);
+			break;
+		case 15:
+			spielfigur.setLocation(252, 420);
+			break;
+		case 16:
+			spielfigur.setLocation(184, 420);
+			break;
+		case 17:
+			spielfigur.setLocation(120, 420);
+			break;
+		case 18:
+			spielfigur.setLocation(52, 420);
+			break;
+		case 19:
+			spielfigur.setLocation(52, 350);
+			break;
+			
+		case 20:
+			spielfigur.setLocation(52, 284);
+			break;
+		case 21:
+			spielfigur.setLocation(120, 284);
+			break;
+		case 22:
+			spielfigur.setLocation(184, 284);
+			break;
+		case 23:
+			spielfigur.setLocation(252, 284);
+			break;
+		case 24:
+			spielfigur.setLocation(318, 284);
+			break;
+		case 25:
+			spielfigur.setLocation(318, 219);
+			break;
+		case 26:
+			spielfigur.setLocation(318, 149);
+			break;
+		case 27:
+			spielfigur.setLocation(318, 83);
+			break;
+		case 28:
+			spielfigur.setLocation(318, 18);
+			break;
+		case 29:
+			spielfigur.setLocation(385, 18);
+			break;
+			
+		case 30:
+			spielfigur.setLocation(452, 18);
+			break;
+		case 31:
+			spielfigur.setLocation(452, 83);
+			break;
+		case 32:
+			spielfigur.setLocation(452, 149);
+			break;
+		case 33:
+			spielfigur.setLocation(452, 219);
+			break;
+		case 34:
+			spielfigur.setLocation(452, 284);
+			break;
+		case 35:
+			spielfigur.setLocation(518, 284);
+			break;
+		case 36:
+			spielfigur.setLocation(585, 284);
+			break;
+		case 37:
+			spielfigur.setLocation(653, 284);
+			break;
+		case 38:
+			spielfigur.setLocation(718, 284);
+			break;
+		case 39:
+			spielfigur.setLocation(718, 350);
+			break;
+			
+		case 40:
+			spielfigur.setLocation(718, 420);
+			break;
+		case 41:
+			spielfigur.setLocation(653, 420);
+			break;
+		case 42:
+			spielfigur.setLocation(585, 420);
+			break;
+		case 43:
+			spielfigur.setLocation(518, 420);
+			break;
+		case 44:
+			spielfigur.setLocation(452, 420);
+			break;
+		case 45:
+			spielfigur.setLocation(452, 479);
+			break;
+		case 46:
+			spielfigur.setLocation(452, 547);
+			break;
+		case 47:
+			spielfigur.setLocation(452, 610);
+			break;
+		case 48:
+			spielfigur.setLocation(452, 680);
+			break;
+		case 49:
+			spielfigur.setLocation(385, 680);
+			break;
+			
+		case 111:
+			spielfigur.setLocation(384, 618);
+			break;
+		case 112:
+			spielfigur.setLocation(384, 550);
+			break;
+		case 113:
+			spielfigur.setLocation(384, 486);
+			break;
+		case 114:
+			spielfigur.setLocation(384, 418);
+			break;
+			
+		case 121:
+			spielfigur.setLocation(118, 350);
+			break;
+		case 122:
+			spielfigur.setLocation(185, 350);
+			break;
+		case 123:
+			spielfigur.setLocation(251, 350);
+			break;
+		case 124:
+			spielfigur.setLocation(318, 350);
+			break;
+			
+		case 131:
+			spielfigur.setLocation(384, 82);
+			break;
+		case 132:
+			spielfigur.setLocation(384, 150);
+			break;
+		case 133:
+			spielfigur.setLocation(384, 218);
+			break;
+		case 134:
+			spielfigur.setLocation(384, 282);
+			break;
+			
+		case 141:
+			spielfigur.setLocation(651, 350);
+			break;
+		case 142:
+			spielfigur.setLocation(585, 350);
+			break;
+		case 143:
+			spielfigur.setLocation(518, 350);
+			break;
+		case 144:
+			spielfigur.setLocation(450, 350);
+			break;
+		}
+
+	}
+	
 	/**
 	 * @wbp.parser.entryPoint
 	 */
@@ -35,32 +269,18 @@ public class GUI {
 		shlMeadn.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurRot.png"));
 		shlMeadn.setSize(816, 839);
 		shlMeadn.setText("MEADN");
-		
-//		Label lblNewLabel = new Label(shlMeadn, SWT.NONE);
-//		lblNewLabel.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\Spielfeld.png"));
-//		lblNewLabel.setBounds(0, 0, 800, 800);
-		
+
+		Label lblNewLabel_1 = new Label(shlMeadn, SWT.NONE);
+		lblNewLabel_1
+				.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurSchwarz.png"));
+		lblNewLabel_1.setBounds(452, 349, 31, 66);
+
+		Label lblNewLabel = new Label(shlMeadn, SWT.NONE);
+		lblNewLabel.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\Spielfeld.png"));
+		lblNewLabel.setBounds(0, 0, 800, 800);
+
 		shlMeadn.open();
 		shlMeadn.layout();
-	}
-	
-
-	public static void setzeSpielfigur(int spielernummer, int figurnummer, int feldnummer) {
-		Label spielfigur = spielfiguren.get(spielernummer * 10 + figurnummer);
-		switch (feldnummer) {
-		case -11:
-			spielfigur.setBounds(48, 616, 33, 60);
-			break;
-		case -12:
-			spielfigur.setBounds(117, 617, 33, 60);
-			break;
-		case -13:
-			spielfigur.setBounds(51, 681, 33, 60);
-			break;
-		case -14:
-			spielfigur.setBounds(117, 682, 33, 60);
-			break;
-		}
 	}
 
 	public static void zeigeSpielfeld() {
@@ -172,28 +392,41 @@ public class GUI {
 	}
 
 	public static void erzeugeSpielfigur(int spielernummer, int figurnummer) {
-		Label label = new Label(shlMeadn, SWT.NONE);
-		
-		switch (spielernummer) {
-		case 1:
-			label.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurSchwarz.png"));
-			break;
-		case 2:
-			label.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurGelb.png"));
-			break;
-		case 3:
-			label.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurGruen.png"));
-			break;
-		case 4:
-			label.setImage(SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurRot.png"));
-			break;
-		}
-		
-		spielfiguren.put(spielernummer * 10 + figurnummer, label);
-		
+		Canvas spielfigur = new Canvas(shlMeadn, SWT.TRANSPARENT);
+		spielfigur.setLocation(116, 180);
+		spielfigur.setSize(127, 159);
+		spielfigur.addPaintListener(new PaintListener() {
+			public void paintControl(PaintEvent e) {
+				switch (spielernummer) {
+				case 1:
+					e.gc.drawImage(
+							SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurSchwarz.png"),
+							0, 0);
+					break;
+				case 2:
+					e.gc.drawImage(
+							SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurGelb.png"), 0,
+							0);
+					break;
+				case 3:
+					e.gc.drawImage(
+							SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurGruen.png"), 0,
+							0);
+					break;
+				case 4:
+					e.gc.drawImage(
+							SWTResourceManager.getImage("C:\\Users\\philt\\git\\maedn\\MAEDN\\img\\FigurRot.png"), 0,
+							0);
+					break;
+				}
+			}
+		});
+
+		spielfiguren.put(spielernummer * 10 + figurnummer, spielfigur);
+
 	}
-	
-	public static void rad(){
+
+	public static void rad() {
 		while (!shlMeadn.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
